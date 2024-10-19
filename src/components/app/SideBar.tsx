7b33c327868ae232ee83dc429dd7457d6125fe03
Mohammed Tirichine <mohtiri718@gmail.com>
2024-10-20 00:11:27 +0100

    Logout functionnality

--- src/components/app/SideBar.tsx
+++ src/components/app/SideBar.tsx
@@ -1,15 +1,23 @@
+'use client';
+
 import Image from 'next/image';
 import Logo from '@/assets/logo.svg';
+import Logout from '@/assets/logout.svg';
 import Link from 'next/link';
 import SideBarCategory from './SidaBarCategory';
 import { kpis, machines } from '@/config/consts';
+import { useContext } from 'react';
+import { AuthContext } from '@/context/AuthContext';
 
 export default function SideBar() {
+    const { removeCurrentUser } = useContext(AuthContext);
+
     return (
         <aside className="flex flex-col items-stretch w-72 bg-[#F8FAFC]">
-            <Link className="p-4" href="/home">
+            <Link className="px-4 py-6" href="/home">
                 <Image src={Logo} alt="Logo" />
             </Link>
+            <hr />
             <SideBarCategory
                 title="Main KPIs"
                 elements={kpis.map((kpi) => ({
@@ -18,6 +26,8 @@ export default function SideBar() {
                     route: `/home/kpis/${kpi.id}`,
                 }))}
             />
+            <div className="h-6"></div>
+            <hr />
             <SideBarCategory
                 title="Machine Types"
                 elements={machines.map((machine) => ({
@@ -26,6 +36,15 @@ export default function SideBar() {
                     route: `/home/${machine.id}`,
                 }))}
             />
+            <button
+                className="flex items-center gap-5 border border-red-500 rounded-lg m-4 px-4 py-2"
+                onClick={removeCurrentUser}
+            >
+                <Image src={Logout} alt="Logout" />
+                <span className="text-red-500 text-lg font-medium">
+                    Log out
+                </span>
+            </button>
         </aside>
     );
 }
